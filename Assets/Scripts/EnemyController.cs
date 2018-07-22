4c33155e6478a640e7346f8287cdfc71d9755f4d
DaiDaiPc\Daiki <21131157dy@tama.ac.jp>
2018-07-22 19:24:50 +0900

    敵の移動の設定まで完了

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    float speed = 10.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(-1 * transform.forward * Time.deltaTime * speed);
+		
+	}
+}
